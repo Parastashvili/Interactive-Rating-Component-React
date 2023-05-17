998a1a263addddeb6e102ec7a9dcb43757f33877
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-05-17 22:25:31 +0400

    mobile version done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,93 @@
+import "./App.css";
+import React, { useState } from "react";
+import starIco from "./images/icon-star.svg";
+import feedBackIco from "./images/illustration-thank-you.svg";
+
+const thanksForm = {
+  thanksText: "Thank you!",
+  thanksParagraph:
+    "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!",
+};
+const rateUs = {
+  question: "How did we do?",
+  description:
+    "Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!",
+  points: [1, 2, 3, 4, 5],
+};
+let score = undefined;
 function App() {
+  const [showQuestion, setShowQuestion] = useState(true);
+  const [showThanks, setShowThanks] = useState(false);
+  const handleQuestionSubmit = () => {
+    if (score == undefined) {
+      alert("Please select a rating");
+    } else {
+      setShowQuestion(false);
+      setShowThanks(true);
+    }
+  };
   return (
-    <div>
-      This is parent Component
-      <ChildComponent
-        name="Badu"
-        age={34}
-        food={["kanchi", "pelmeni"]}
-        images={{
-          profile:
-            "https://fastly.picsum.photos/id/1077/300/300.jpg?hmac=c7s9EbUHIZn4bUD_xkYc7DndRJGdnkoApLqPmhCqpcg",
-          avatar:
-            "https://fastly.picsum.photos/id/419/300/600.jpg?hmac=m5TqMJwIxScdnw_RMU_PUvonwU6lzgLBaKsOWUkSFPw",
-        }}
-      />
+    <main className="innerContainer">
+      {showQuestion && (
+        <GenerateQuestion
+          question={rateUs.question}
+          dsc={rateUs.description}
+          points={rateUs.points}
+          onSubmit={handleQuestionSubmit}
+        />
+      )}
+      {showThanks && <FeedBack />}
+    </main>
+  );
+}
+function FeedBack() {
+  return (
+    <div className="feedBack">
+      <img className="tanksImg" src={feedBackIco} alt="thank-you" />
+      <h5 className="scoreField">You selected {score} out of 5</h5>
+      <h2 className="thanksText">{thanksForm.thanksText}</h2>
+      <p className="thanksParagraph">{thanksForm.thanksParagraph}</p>
+    </div>
+  );
+}
+function GenerateQuestion(props) {
+  const { question, dsc, points, onSubmit } = props;
+  return (
+    <div className="questionContainer">
+      <div className="starBox">
+        <img className="starImg" src={starIco} alt="star-icon" />
+      </div>
+      <h1 className="questionText">{question}</h1>
+      <p className="questionDsc">{dsc}</p>
+      <RateScores points={points} />
+      <button className="submitButton" onClick={onSubmit}>
+        SUBMIT
+      </button>
+    </div>
+  );
+}
+function RateScores(props) {
+  const [selectedPoint, setSelectedPoint] = useState(null);
+  const handlePointSelect = (point) => {
+    setSelectedPoint(point);
+  };
+  return (
+    <div className="rateScores">
+      {props.points.map((point, index) => (
+        <button
+          className={`eachScore ${selectedPoint === point ? "selected" : ""}`}
+          key={index}
+          value={point}
+          onClick={() => {
+            handlePointSelect(point);
+            score = point;
+          }}
+        >
+          {point}
+        </button>
+      ))}
     </div>
   );
 }
 
 export default App;
-
-function ChildComponent(props) {
-  return (
-    <div>
-      My name is {props.name} and I am {props.age} years old, My favourite food
-      is : {props.food[0]} and {props.food[1]}
-      <br />
-      <img src={props.images.profile} /> <img src={props.images.avatar} />{" "}
-    </div>
-  );
-}
